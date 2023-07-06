4478614a4ceedc9edd8c3dd41cc9374229039d81
wancynthianjeri <wanjirucynthianjeri@gmail.com>
2023-07-06 08:07:11 +0300

    commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import Login from './Login';
+import Products from './Products/products';
+
+function App() {
+  return (
+    <div>
+      <Login/>
+        <Products/>
+      
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Products/products.js
@@ -0,0 +1,37 @@
+import { wait } from "@testing-library/user-event/dist/utils";
+import React, {useState, useEffect} from "react";
+const Products =()=>{
+  const [products, setProducts] = useState([]);
+  const [loading,setLoading] = useState(false);
+  useEffect(()=>{
+    (async()=>{
+      await getProducts();
+    })()
+  },[])
+console.log(products);
+  const getProducts = async()=>{
+    try {
+      setLoading(true);
+      const response = await fetch('https://dummyjson.com/products')
+      const result = await response.json();
+      setProducts(result.products)
+      setLoading(false);
+    } catch (error) {
+     console.log(error.message);
+    }
+  }
+  if(loading){
+    return <h2>loading...</h2>
+  }
+  return(
+    <div>
+      <h1>All Products</h1>
+      {products.map((item) =>(
+        <div key={item.id}>
+        <h2>{item.title}</h2>
+        </div>
+      ))}
+    </div>
+  );
+};
+export default Products
